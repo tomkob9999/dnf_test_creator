981f9025b712e3f595d74518f37df1fe1f1d91e3
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2023-12-10 08:07:55 +0900

    1.4
    Enabled to handle random irrelevant variables.  Without the fix, this didn’t return any results.  The performance has become non-linear (O(n^2).

--- DNF_RegressionSolver.py
+++ DNF_RegressionSolver.py
@@ -1,38 +1,38 @@
 # Author: tomio kobayashi
-# Version: 1.3.2
-# Date: 2023/12/09
-
+# Version: 1.4
+# Date: 2023/12/10
 
 import itertools
 
+import re
+class rdict(dict):
+    def __getitem__(self, key):
+        p = re.compile(key)
+        r = [ v for k,v in self.items() if p.search(k) ]
+        return r if len(r) > 0 else []
+    def get(self, k, d=None):
+        p = re.compile(k)
+        r = [(k, v) for k, v in self.items() if p.search(k)]
+        return r if len(r) > 1 else []
+    
+    
 class DNF_Regression_solver:
 
-    def solve(file_path, good_thresh=0.7, allow_unmatch_for_good=False, max_dnf_len = 15):
-        # def read_tab_delimited_file_to_list(file_path):
-        #     with open(file_path, 'r') as f:
-        #         return [line.strip().split('\t') for line in f]
+    def solve(file_path, good_thresh=0.7, allow_unmatch_for_good=False, max_dnf_len = 6):
 
         # Example usage:
         # file_path = '/kaggle/input/dnf-regression/dnf_regression.txt'
     #     file_path = '/kaggle/input/tomio5/dnf_regression.txt'
-        # inp = read_tab_delimited_file_to_list(file_path)
 
-#         good_thresh = 0.7 # TRESH_HOLD THAT THE FALSE MATCH SHOULD BE CONSIDERED AS GOOD
-        
         with open(file_path, 'r') as f:
             inp = [line.strip().split('\t') for line in f]
 
-
-        # print(inp)
-
-        # print(inp[1])
-
         numvars = len(inp[1])-1
-        # print("numvars", numvars)
 
         if numvars - 1 < max_dnf_len:
             max_dnf_len = numvars - 1
         dic = dict()
+        
         true_list = []
         false_list = []
         for i in range(1, len(inp), 1):
@@ -46,7 +46,10 @@ class DNF_Regression_solver:
             else:
                 false_list.append(s)
 
+        rdic = rdict(dic)
+        
         zeros = "0" * numvars 
+        nonzeros = "." * numvars 
 
         dnf_perf = list()
         dnf_good = list()
@@ -56,7 +59,6 @@ class DNF_Regression_solver:
             len_dnf = s + 1
             l = [ii for ii in range(numvars)]
             p_list = list(itertools.combinations(l, len_dnf))
-        #     print("p_list", p_list)
 
             true_test_pass = True
             for i in range(len(p_list)):
@@ -64,36 +66,36 @@ class DNF_Regression_solver:
                 match_and_break = False
                 for p in raw_perf:
                     s = sorted(set(p).intersection(set(list(p_list[i]))))
-#                     print("compare p", p)
-#                     print("compare list(p_list[i])", list(p_list[i]))
-#                     print("compare s", s)
                     if len(p) == len(s):
-#                         print("matched and break")
                         match_and_break = True
                         break
                 if match_and_break:
                     continue
-                search_str = zeros
+                search_str = nonzeros
                 for jj in p_list[i]:
                     search_str = search_str[0:jj] + "1" + search_str[jj+1:len(search_str)]
-        #         print("search_str", search_str)
-        #         print("dic[search_str]", dic[search_str])
-                if dic[search_str] == "0":
+                rdic_res = rdic[search_str]
+                if len(rdic_res) == 0:
                     continue
-
+                if len(rdic_res) == 0 or len([f for f in rdic_res if f == "0"]) > 0:
+                    continue
+                      
                 search_str2 = search_str
                 count_false = 0
                 found_true = False
                 for jj in p_list[i]:
                     search_str2 = search_str[0:jj] + "0" + search_str[jj+1:len(search_str)]
 
-        #             print("search_str2", search_str2)
-        #             print("dic[search_str2]", dic[search_str2])
-                    if dic[search_str2] == "1":
+                    rdic_res = rdic[search_str2]
+                    if len(rdic_res) == 0:
+                        continue
+                    if len(rdic_res) == 0 or len([f for f in rdic_res if f == "0"]) == 0:
                         found_true = True
                         break
+                        
+            
+                        
                     count_false += 1
-        #         print("count_false", count_false)
                 if not allow_unmatch_for_good:
                     if found_true :
                         continue
@@ -103,38 +105,21 @@ class DNF_Regression_solver:
                 elif float(count_false)/float(len_dnf) >= good_thresh:
                     dnf_good.append([inp[0][ii] for ii in p_list[i]])
 
-        # print("dnf_perf", dnf_perf)
-        # print("dnf_good", dnf_good)
-
-
         print("")
-        print("DNF with perfect match (1.0) - " + str(len(dnf_perf)))
+        dnf_perf.extend(dnf_good)
+        print("DNF with high confidence (over " + str(good_thresh) +") - " + str(len(dnf_perf)))
         print("--------------------------------")
-        # Raw Pattern
-#         for s in sorted(dnf_perf):
-#             print(s)
+
         if len(dnf_perf) > 0:
             print("(" + ") | (".join([" & ".join(a) for a in dnf_perf]) + ")")
-        # Processed Pattern
-        # for c in sorted(winsets):
-        #     print(', '.join(map(str, c)))
-        print("")
-        print("DNF with good match (over " + str(good_thresh) +") - " + str(len(dnf_good)))
-        print("--------------------------------")
-        
-        if len(dnf_good) > 0:
-            print("(" + ") | (".join([" & ".join(a) for a in dnf_good]) + ")")
-#         for s in sorted(dnf_good):
-#             print(s)
 
         perm_vars = [xx for x in dnf_perf for xx in x]
         not_picked = [inp[0][ii] for ii in range(len(inp[0])-1) if inp[0][ii] not in perm_vars]
 
         print("")
-        print("Variables that were not picked up - " + str(len(not_picked)) + "/" + str(len(perm_vars)))
+        print("Unsolved variables - " + str(len(not_picked)) + "/" + str(numvars))
         print("--------------------------------")
         print(not_picked)
-
 
 
 
