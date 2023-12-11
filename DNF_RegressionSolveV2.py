c23917da7ab6b2c5509885d41d6989ff8251527a
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2023-12-11 14:04:57 +0900

    2.1
    Replaced string manipulation and regular expression with bit operations to improve performance.  also some bug fixes.

--- DNF_RegressionSolveV2.py
+++ DNF_RegressionSolveV2.py
@@ -1,26 +1,33 @@
 # Name: DNF_Regression_solver
 # Author: tomio kobayashi
-# Version: 2.0
-# Date: 2023/12/10
+# Version: 2.1
+# Date: 2023/12/11
 
 import itertools
 
-import re
-class rdict(dict):
-    def __getitem__(self, key):
-        p = re.compile(key)
-        r = [ v for k,v in self.items() if p.search(k) ]
-        return r if len(r) > 0 else []
-    def get(self, k, d=None):
-        p = re.compile(k)
-        r = [(k, v) for k, v in self.items() if p.search(k)]
-        return r if len(r) > 1 else []
+# import re
+# class rdict(dict):
+#     def __getitem__(self, key):
+#         p = re.compile(key)
+#         r = [ v for k,v in self.items() if p.search(k) ]
+#         return r if len(r) > 0 else []
+#     def get(self, k, d=None):
+#         p = re.compile(k)
+#         r = [(k, v) for k, v in self.items() if p.search(k)]
+#         return r if len(r) > 1 else []
     
     
 class DNF_Regression_solver:
 #     This version has no good matches
 # Instead, all true matches are first added, and some are removed when 
 # false unmatch exists and there is no corresponding other rule
+    def convTuple2bin(t, width):
+        i = 1
+        ret = 0
+        for x in t:
+            ii = i << width - x - 1
+            ret = ret | ii
+        return ret
 
     def solve(file_path):
 
@@ -43,21 +50,16 @@ class DNF_Regression_solver:
             for j in range(len(inp[i]) - 1):
                 s += inp[i][j]
             truefalse = inp[i][len(inp[i]) - 1]
-            dic[s] = truefalse
+            dic[int(s, 2)] = truefalse
             if truefalse == '1':
                 true_list.append(s)
             else:
                 false_list.append(s)
 
-        rdic = rdict(dic)
-        
-        zeros = "0" * numvars 
-        nonzeros = "." * numvars 
-
         dnf_perf = list()
         raw_perf = list()
-        
-        failed_false_tests = 0
+
+        raw_perf2 = list()
         for s in range(max_dnf_len):
             len_dnf = s + 1
             l = [ii for ii in range(numvars)]
@@ -65,63 +67,56 @@ class DNF_Regression_solver:
 
             true_test_pass = True
             for i in range(len(p_list)):
-
                 match_and_break = False
-                for p in raw_perf:
-                    s = sorted(set(p).intersection(set(list(p_list[i]))))
-                    if len(p) == len(s):
+                b = DNF_Regression_solver.convTuple2bin(p_list[i], numvars)
+                for p in raw_perf2:
+                    if p == b & p:
                         match_and_break = True
                         break
                 if match_and_break:
                     continue
-                search_str = nonzeros
-                for jj in p_list[i]:
-                    search_str = search_str[0:jj] + "1" + search_str[jj+1:len(search_str)]
-                rdic_res = rdic[search_str]
-                if len(rdic_res) == 0:
+                r = [v for k,v in dic.items() if k & b == b]
+                if len(r) == 0:
                     continue
-                if len([f for f in rdic_res if f == "0"]) > 0:
+                if len([f for f in r if f == "0"]) > 0:
                     continue
-                      
-                search_str2 = search_str
-                count_false = 0
-                found_true = False
+
                 raw_perf.append([ii for ii in p_list[i]])
+                raw_perf2.append(b)
 
         fake_raw = []
         for i in range(len(raw_perf)):
 
-            search_str = nonzeros
-            for jj in raw_perf[i]:
-                search_str = search_str[0:jj] + "1" + search_str[jj+1:len(search_str)]
+            b = raw_perf2[i]
             count_false = 0
             found_true = False
 
             for jj in raw_perf[i]:
-                search_str2 = search_str[0:jj] + "0" + search_str[jj+1:len(search_str)]
-
-                rdic_res = rdic.get(search_str2)
+                jj2 = 1 << (numvars - jj - 1)
+                jj3 = b ^ jj2
+                rdic_res = [(k, v) for k,v in dic.items() if k & jj2 == jj2 and v == "1"]
                 if len(rdic_res) == 0:
                     continue
+                is_any_match = False
                 for f in rdic_res:
                     if f[1] == "0":
                         continue
-                    is_any_match = False
-                    for kk in raw_perf:
+                    for kk in raw_perf2:
                         is_match = True
-                        for mm in kk:
-                            if f[0][mm:mm+1] == "0":
-                                is_match = False
-                                break
+                        if kk != kk & f[0]:
+                            is_match = False
+                            break
                         if is_match:
                             is_any_match = True
                             break
-                    if not is_any_match:
-                        fake_raw.append()
+                    if is_any_match:
+                        break
+                if not is_any_match:
+                    fake_raw.append(raw_perf[i])
         for f in fake_raw:
             for iii in range(len(raw_perf)):
                 if f == raw_perf[iii]:
-                    raw_perf.pop[iii]
+                    raw_perf.pop(iii)
                     break
 
         for dn in raw_perf:
@@ -142,7 +137,6 @@ class DNF_Regression_solver:
         print("Unsolved variables - " + str(len(not_picked)) + "/" + str(len(perm_vars)))
         print("--------------------------------")
         print(not_picked)
-
 
 
 file_path = '/kaggle/input/tomio2/dnf_regression.txt'
