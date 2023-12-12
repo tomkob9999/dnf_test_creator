2534e482d3ab418002176d3db9a26cb76d0362ac
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2023-12-12 17:59:33 +0900

    2.1.1
    The negative condition option is added.  If enabled, negative conditions are automatically added.

--- DNF_RegressionSolveV2.py
+++ DNF_RegressionSolveV2.py
@@ -1,12 +1,12 @@
 # Name: DNF_Regression_solver
 # Author: tomio kobayashi
-# Version: 2.1.1
+# Version: 2.1.2
 # Date: 2023/12/11
 
 import itertools
     
 class DNF_Regression_solver:
-#     This version has no good matches
+# This version has no good matches
 # Instead, all true matches are first added, and some are removed when 
 # false unmatch exists and there is no corresponding other rule
     def convTuple2bin(t, width):
@@ -17,7 +17,12 @@ class DNF_Regression_solver:
             ret = ret | ii
         return ret
 
-    def solve(file_path):
+    def solve(file_path, check_negative=False, max_dnf_len=6, drop_fake=False):
+# file_path: input file in tab-delimited text
+# check_negative: enable to check the negative conditions or not.  This one is very heavy.
+# max_dnf_len: max length of AND clauses in output DNF.  
+#       Increasing this one is heavey especially if check_negative is True
+# drop_fake: enable to drop the clause that met the true condition, but not false condition.  This one is heavy.
 
         # Example usage:
         # file_path = '/kaggle/input/dnf-regression/dnf_regression.txt'
@@ -28,7 +33,17 @@ class DNF_Regression_solver:
 
         numvars = len(inp[1])-1
 
-        max_dnf_len = numvars - 1
+        if check_negative:
+            for i in range(numvars):
+                inp[0].insert(i+numvars, "n_" + inp[0][i])
+            for j in range(1, len(inp), 1):
+                for i in range(numvars):
+                    inp[j].insert(i+numvars,"0" if inp[j][i] == "1" else "1")
+            numvars *= 2
+
+        if max_dnf_len > numvars - 1:
+            max_dnf_len = numvars - 1
+            
         dic = dict()
         
         true_list = []
@@ -72,36 +87,37 @@ class DNF_Regression_solver:
                 raw_perf.append([ii for ii in p_list[i]])
                 raw_perf2.append(b)
 
-        fake_raw = []
-        for i in range(len(raw_perf)):
+        if drop_fake:
+            fake_raw = []
+            for i in range(len(raw_perf)):
 
-            b = raw_perf2[i]
-            count_false = 0
-            found_true = False
+                b = raw_perf2[i]
+                count_false = 0
+                found_true = False
 
-            for jj in raw_perf[i]:
-                jj2 = 1 << (numvars - jj - 1)
-                jj3 = b ^ jj2
-                rdic_res = [(k, v) for k,v in dic.items() if k & jj3 == jj3 and v == "1"]
-                rdic_resxx = [(bin(k), v) for k,v in dic.items() if k & jj3 == jj3 and v == "1"]
-                if len(rdic_res) == 0:
-                    continue
-                is_any_match = False
-                for f in rdic_res:
-                    for kk in raw_perf2:
-                        if kk == kk & f[0]:
-                            is_any_match = True
+                for jj in raw_perf[i]:
+                    jj2 = 1 << (numvars - jj - 1)
+                    jj3 = b ^ jj2
+                    rdic_res = [(k, v) for k,v in dic.items() if k & jj3 == jj3 and v == "1"]
+                    rdic_resxx = [(bin(k), v) for k,v in dic.items() if k & jj3 == jj3 and v == "1"]
+                    if len(rdic_res) == 0:
+                        continue
+                    is_any_match = False
+                    for f in rdic_res:
+                        for kk in raw_perf2:
+                            if kk == kk & f[0]:
+                                is_any_match = True
+                                break
+                        if is_any_match:
                             break
-                    if is_any_match:
+                    if not is_any_match:
+                        fake_raw.append(raw_perf[i])
                         break
-                if not is_any_match:
-                    fake_raw.append(raw_perf[i])
-                    break
-        for f in fake_raw:
-            for iii in range(len(raw_perf)):
-                if f == raw_perf[iii]:
-                    raw_perf.pop(iii)
-                    break
+            for f in fake_raw:
+                for iii in range(len(raw_perf)):
+                    if f == raw_perf[iii]:
+                        raw_perf.pop(iii)
+                        break
 
         for dn in raw_perf:
             dnf_perf.append([inp[0][ii] for ii in dn])
